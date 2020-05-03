ff7d15e0c6270c32022766ef5e76d7ee6328cfa2
Şahin Alçın <sahinalcin@outlook.com>
2020-05-03 21:25:56 +0300

    Update client.java

--- client.java
+++ client.java
@@ -1,4 +1,8 @@
+package esarat.screenShare;
+
 import java.awt.image.BufferedImage;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
@@ -6,10 +10,14 @@ import java.net.Socket;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class client {
+
     Socket socket;
     boolean isRunning = true;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ekrani gormesi icin asagidaki bilgileri giriniz:");
@@ -18,49 +26,95 @@ public class client {
         System.out.println("IP:");
         String ipAdd = sc.next();
         client clnt = new client();
-        clnt.start(ipAdd, Integer.parseInt(port));
-        
+        System.out.println("password:");
+        String password = sc.next();
+        clnt.start(ipAdd, Integer.parseInt(port), password);
+
         while (clnt.isRunning) {
             System.out.println("options:\n1:to end");
             if (sc.hasNext()) {
-               if(sc.nextInt() == 1){
-                   clnt.close();
-               }
+                if (sc.nextInt() == 1) {
+                    clnt.close();
+                }
             }
         }
 
+    }
+
+    public void close() {
+        try {
+            if (socket != null) {
+                isRunning = false;
+                socket.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start(String serverAddr, int port, String password) {
+        JFrame frame = new JFrame();
+        ImagePanel panel = new ImagePanel();
+        frame.setResizable(true);
+        frame.add(panel);
+        frame.pack();
+         frame.setVisible(true);
+        try {
+            BufferedImage image;
+            while (isRunning) {
+                socket = new Socket(serverAddr, port);
+                boolean result = checkPassword(socket, password);
+                System.out.println(result);
+                if (result) {
+                    ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+                    image = ImageIO.read(inputStream);
+                    panel.setImg(image);
+                    panel.repaint();
+                    socket.close();
+                   
+                } else {
+                     frame.setVisible(false);
+                    isRunning = false;
+                    System.out.println("Wrong password");
+                }
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean checkPassword(Socket socket, String password) {
+        sendPassword(socket, password);
+        return getPasswordChecker(socket);
 
     }
-    private void close(){
-		try {
-            isRunning= false;
-			socket.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 
-    private void start(String serverAddr, int port) {
-		JFrame frame = new JFrame();
-		ImagePanel panel = new ImagePanel();
-		frame.setResizable(true);
-		frame.add(panel);
-		frame.pack();
-		frame.setVisible(true);
-		try {
-			BufferedImage image;
-			while(isRunning){
-				socket = new Socket(serverAddr, port);
-				ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
-				image = ImageIO.read(inputStream);
-				panel.setImg(image);
-				panel.repaint();
-				socket.close();
-			}
+    public void sendPassword(Socket socket, String password) {
+        DataOutputStream dOut;
+        try {
+            dOut = new DataOutputStream(socket.getOutputStream());
+            dOut.writeUTF(password);
+            dOut.flush(); // Send off the data
+        } catch (IOException ex) {
+            Logger.getLogger(client.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
+    }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		} 
-	}
+    public boolean getPasswordChecker(Socket socket) {
+        DataInputStream dIn;
+        try {
+            dIn = new DataInputStream(socket.getInputStream());
+            boolean result = dIn.readBoolean();
+            System.out.println(result);
+            return result;
+        } catch (IOException ex) {
+            Logger.getLogger(client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+
+    }
 }
